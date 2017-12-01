116c645ebabf531b109dcec6d74c8ab5be437369
Nema Darban <nema.darban@gmail.com>
2017-12-01 08:03:26 -0800

    expanded flow logic -WIP, still framing failure flow before framing success actions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,16 @@
 var wordList = ["elmo","grover","big bird","mr snuffleupagus","count von count","kermit the frog","bert","ernie","cookie monster","oscar the grouch"];
-var userGuess = "";
 var userGuessList = [];
+var userGuessPrint = ""
 var remainingGuesses = 9;
 
+var guessesRemainingElement = document.getElementById("guessesRemaining");
+var characterLinesElement = document.getElementById("characterLines");
+var hintElement = document.getElementById("hint");
+var guessedLettersElement = document.getElementById("guessedLetters");
+
 window.onclick = function () {
+
+
 	//select random word
 	var listPosition = Math.floor((Math.random() * wordList.length));
 	var selectedWord = wordList[listPosition];
@@ -13,24 +20,51 @@ window.onclick = function () {
 	
 
 	//accept guesses
-	document.onkeyup = function(guess) {
+	document.onkeyup = function(event) {
+		var userGuess = event.key;
+		var guessCheck = selectedWord.includes(userGuess);
+		var characterCheck = alphabet.includes(userGuess);
+		var keystroke = 0;
 		
-		var guessCheck = selectedWord.includes(guess);
-		var usedLetterCheck = userGuess.includes(guess);
-		if (guessCheck === true) {
-
+		userGuessList.push(userGuess);
+		
+		if ((guessCheck = true) && (characterCheck = true)){
+			validityCheck = true; 
+		} else {
+			validityCheck = false;
 		}
-		else {
+
+		if (userGuessList.indexOf(userGuess) > -1 ){
+			var usedLetterCheck = true;
+		} else {
+			var usedLetterCheck = false;
+		}
+
+
+
+		if (validityCheck === true) {
+
+		} else {
 			//if letter hasn't been guessed - lower count and add to the list
 			if (usedLetterCheck === false ) {
-				userGuessList.push(guess);
 				remainingGuesses = remainingGuesses - 1;
 			} 
 			//if letter has been guessed don't do anything
 			else { 
+			}
+		}
+		
+		console.log(guessCheck);
+		console.log(userGuess);
+		console.log(userGuessList);
+		console.log(remainingGuesses);
+		console.log(selectedWord);
+		console.log(userGuessPrint);
 
-			};
-		};
+		guessedLettersElement.textContent = "Letters Guessed: " + userGuessList;
+		/*guessesRemainingElement.textContent = "Number of guesses remaining: " + remainingGuesses;*/
+
+
 	}
 
 	//compare guesses
@@ -38,9 +72,6 @@ window.onclick = function () {
 
 
 	//test output
-	console.log(userGuess);
-	console.log(userGuessList);
-	console.log(remainingGuesses);
-	console.log(selectedWord);
+
 
 }
