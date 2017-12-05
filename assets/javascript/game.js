c9844de6535cd6b8b953a1c592a5f25033868c31
Nema Darban <nema.darban@gmail.com>
2017-12-04 23:15:28 -0800

    Corrected Hint Output -Logic for printing hint finalized -Hint images will now be automatically populated based on the selectedWord -Commented out unneeded code for cleanup later
    --Need to put 'Win' action

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,27 @@
 var wordList = ["elmo","grover","big bird","mr snuffleupagus","count von count","kermit the frog","bert","ernie","cookie monster","oscar the grouch"];
-var userGuessList = [];
-var userGuessPrint = ""
-var remainingGuesses = 9;
-var blankHint = "";
+var userGuessPrint = "";
+var letterToPrint = "";
 var guessesRemainingElement = document.getElementById("guessesRemaining");
 var characterLinesElement = document.getElementById("characterLines");
 var hintElement = document.getElementById("hint");
 var guessedLettersElement = document.getElementById("guessedLetters");
+var imageLinks = {
+	"elmo": "elmo.jpg",
+	"grover": "grover.jpg",
+	"big bird": "bigBird.jpg",
+	"mr snuffleupagus": "mrSnuffleupagus.jpg",
+	"count von count": "countVonCount.png",
+	"kermit the frog": "kermitTheFrog.jpg",
+	"bert": "bert.png",
+	"ernie": "ernie.png",
+	"cookie monster": "cookieMonster.jpg",
+	"oscar the grouch": "oscarTheGrout.jpg"};
+
 
 window.onclick = function () {
-
-
+	var blankHint = "";
+	var userGuessList = [];
+	var remainingGuesses = 9;
 	//select random word
 	var listPosition = Math.floor((Math.random() * wordList.length));
 	var selectedWord = wordList[listPosition];
@@ -22,7 +33,23 @@ window.onclick = function () {
 		console.log(spacesRemoved[i]);
 	}
 */
+	for (i = 0; i < selectedWord.length; i++) {
+		letterToPrint = selectedWord.charAt(i);
+		if ($.inArray(letterToPrint, userGuessList) != -1){
+			blankHint += letterToPrint;
+		} else if (letterToPrint === " ") {
+			blankHint += "\xa0";
+		} else {
+			blankHint += " _ ";
+		}
+	}
+	var imageToGet = imageLinks[selectedWord];
 
+	hintElement.innerHTML = "<img src='./assets/images/"+imageToGet+"' class='img-rounded' alt='"+selectedWord+"'></img>";
+	//hintElement.setAttribute("background-image", "url(./assets/images/bert.png");
+	characterLinesElement.textContent = blankHint;
+	guessedLettersElement.textContent = "Letters Guessed: " + userGuessList;
+	guessesRemainingElement.textContent = "Guesses Remaining: " + remainingGuesses;
 	//accept guesses
 	document.onkeyup = function(event) {
 		var userGuess = event.key;
@@ -31,6 +58,7 @@ window.onclick = function () {
 		var characterCheck = alphabet.includes(userGuess);
 		var keystroke = 0;
 		var hasLetterBeenGuessed;
+		blankHint = "";
 
 		// If the character is in the guessed list and its valid set BeenGuessed to true
 		if (userGuessList.indexOf(userGuess) > -1 && characterCheck === true){
@@ -57,18 +85,29 @@ window.onclick = function () {
 
 		//if the letter in the guessed list is in the word, print the letter otherwise print the blank line
 		for (i = 0; i < selectedWord.length; i++) {
-			var letterToPrint = selectedWord.charAt(i);
-			
-			if (selectedWord.includes(letterToPrint)){
-
+			letterToPrint = selectedWord.charAt(i);
+			if ($.inArray(letterToPrint, userGuessList) != -1){
+				blankHint += letterToPrint;
+			} else if (letterToPrint === " ") {
+				blankHint += "\xa0";
+			} else {
+				blankHint += " _ ";
 			}
-			if (letterToPrint !== " ") {
+
+			
+
+
+			/*if (selectedWord.includes(letterToPrint) === true){
+				console.log("DIGITTY");
+			} else {
+				console.log("doopy");
+			}*/
+/*			if (letterToPrint !== " ") {
 				blankHint = blankHint + "_ ";
 			} else {
 				blankHint = blankHint + "  ";
-			}
+			}*/
 		}
-
 
 		
 		console.log("Guess Check " + guessCheck);
@@ -81,6 +120,7 @@ window.onclick = function () {
 		console.log("user guess print " + userGuessPrint);
 		/*console.log(spacesRemoved);*/
 		console.log(blankHint);
+		characterLinesElement.textContent = blankHint
 		guessedLettersElement.textContent = "Letters Guessed: " + userGuessList;
 		guessesRemainingElement.textContent = "Guesses Remaining: " + remainingGuesses;
 		/*guessesRemainingElement.textContent = "Number of guesses remaining: " + remainingGuesses;*/
